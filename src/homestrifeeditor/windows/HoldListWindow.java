045af2a621b052929f9339230065b0a4f8f4fb24
Joshua Luke <joshuadluke@gmail.com>
2014-04-01 10:59:24 -0400

    Clean up code a bit; add about window

--- src/homestrifeeditor/windows/HoldListWindow.java
+++ src/homestrifeeditor/windows/HoldListWindow.java
@@ -49,7 +49,6 @@ import javax.swing.tree.TreePath;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.soap.Text;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -134,162 +133,162 @@ public class HoldListWindow extends JFrame implements ActionListener {
 	private void createMenuBar()
     {
         JMenuBar menuBar;
-        JMenu file;
-        JMenu newObject;
-        JMenuItem newGraphic;
-        JMenuItem newTerrain;
-        JMenuItem newPhysicsObject;
-        JMenuItem newFighter;
-        JMenuItem generate;
-        JMenuItem open;
-        JMenuItem save;
-        JMenuItem saveAs;
-        JMenuItem importAnimation;
-        JMenuItem setExeLocation;
-        JMenu edit;
-        JMenuItem undo;
-        JMenuItem redo;
-        JMenuItem cut;
-        JMenuItem copy;
-        JMenuItem paste;
-        JMenuItem delete;
-        JMenuItem selectAll;
-        JMenu object;
-        JMenuItem objectAttributes;
-        JMenuItem eventHolds;
-        JMenu help;
-        JMenuItem helpContent;
-        JMenuItem about;
+        JMenu fileMenu;
+        JMenu newObjectMenu;
+        JMenuItem newGraphicItem, newTerrainItem, newPhysicsObjectItem, newFighterItem;
+        JMenuItem generateItem, openItem, saveItem, saveAsItem, importAnimationItem, setExeLocationItem;
+        
+        JMenu editMenu;
+        JMenuItem undoItem, redoItem, cutItem, copyItem, pasteItem, deleteItem, selectAllItem;
+        
+        JMenu objectMenu;
+        JMenuItem objectAttributesItem, eventHoldsItem;
+        
+        JMenu helpMenu;
+        JMenuItem helpContentItem, aboutItem;
         
         menuBar = new JMenuBar();
         
-        file = new JMenu("File");
-        newObject = new JMenu("New");
+        fileMenu = new JMenu("File");
+        newObjectMenu = new JMenu("New");
         //
-        newGraphic = new JMenuItem("Graphic");
-        newGraphic.setActionCommand("newGraphic");
-        newGraphic.addActionListener(this);
+        newGraphicItem = new JMenuItem("Graphic");
+        newGraphicItem.setActionCommand("newGraphic");
+        newGraphicItem.addActionListener(this);
         //
-        newTerrain = new JMenuItem("Terrain");
-        newTerrain.setActionCommand("newTerrain");
-        newTerrain.addActionListener(this);
+        newTerrainItem = new JMenuItem("Terrain");
+        newTerrainItem.setActionCommand("newTerrain");
+        newTerrainItem.addActionListener(this);
         //
-        newPhysicsObject = new JMenuItem("Physics Object");
-        newPhysicsObject.setActionCommand("newPhysicsObject");
-        newPhysicsObject.addActionListener(this);
+        newPhysicsObjectItem = new JMenuItem("Physics Object");
+        newPhysicsObjectItem.setActionCommand("newPhysicsObject");
+        newPhysicsObjectItem.addActionListener(this);
         //
-        newFighter = new JMenuItem("Fighter");
-        newFighter.setActionCommand("newFighter");
-        newFighter.addActionListener(this);
+        newFighterItem = new JMenuItem("Fighter");
+        newFighterItem.setActionCommand("newFighter");
+        newFighterItem.addActionListener(this);
         //
-        generate = new JMenuItem("Generate...");
-        generate.setActionCommand("generate");
-        generate.addActionListener(this);
+        generateItem = new JMenuItem("Generate...");
+        generateItem.setActionCommand("generate");
+        generateItem.addActionListener(this);
         //
-        open = new JMenuItem("Open...");
-        open.setActionCommand("open");
-        open.addActionListener(this);
+        openItem = new JMenuItem("Open...");
+        openItem.setActionCommand("open");
+        openItem.addActionListener(this);
         //
-        save = new JMenuItem("Save");
-        save.setActionCommand("save");
-        save.addActionListener(this);
-        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+        saveItem = new JMenuItem("Save");
+        saveItem.setActionCommand("save");
+        saveItem.addActionListener(this);
+        saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
         //
-        saveAs = new JMenuItem("Save As...");
-        saveAs.setActionCommand("saveAs");
-        saveAs.addActionListener(this);
+        saveAsItem = new JMenuItem("Save As...");
+        saveAsItem.setActionCommand("saveAs");
+        saveAsItem.addActionListener(this);
         //
-        importAnimation = new JMenuItem("Import Animation");
-        importAnimation.setActionCommand("importAnimation");
-        importAnimation.addActionListener(this);
+        importAnimationItem = new JMenuItem("Import Animation");
+        importAnimationItem.setActionCommand("importAnimation");
+        importAnimationItem.addActionListener(this);
         //
-        setExeLocation = new JMenuItem("Set Game Location");
-        setExeLocation.setActionCommand("exeLocation");
-        setExeLocation.addActionListener(this);
-        //
-        newObject.add(newGraphic);
-        newObject.add(newTerrain);
-        newObject.add(newPhysicsObject);
-        newObject.add(newFighter);
-        file.add(newObject);
-        file.add(generate);
-        file.add(open);
-        file.add(save);
-        file.add(saveAs);
-        file.add(new JSeparator());
-        file.add(importAnimation);
-        file.add(new JSeparator());
-        file.add(setExeLocation);
-        menuBar.add(file);
-        
-        edit = new JMenu("Edit");
-        //
-        undo = new JMenuItem("Undo");
-        undo.setActionCommand("undo");
-        undo.addActionListener(this);
-        undo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_DOWN_MASK));
-        //
-        redo = new JMenuItem("Redo");
-        redo.setActionCommand("redo");
-        redo.addActionListener(this);
-        //
-        cut = new JMenuItem("Cut");
-        cut.setActionCommand("cut");
-        cut.addActionListener(this);
-        cut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK));
-        //
-        copy = new JMenuItem("Copy");
-        copy.setActionCommand("copy");
-        copy.addActionListener(this);
-        copy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
-        //
-        paste = new JMenuItem("Paste");
-        paste.setActionCommand("paste");
-        paste.addActionListener(this);
-        paste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK));
-        //
-        delete = new JMenuItem("Delete");
-        delete.setActionCommand("delete");
-        delete.addActionListener(this);
-        delete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
-        //
-        selectAll = new JMenuItem("Select All");
-        selectAll.setActionCommand("selectAll");
-        selectAll.addActionListener(this);
-        selectAll.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.ALT_DOWN_MASK));
+        setExeLocationItem = new JMenuItem("Set Game Location");
+        setExeLocationItem.setActionCommand("exeLocation");
+        setExeLocationItem.addActionListener(this);
         //
         
-        edit.add(undo);
-        edit.add(redo);
-        edit.add(new JSeparator());
-        edit.add(cut);
-        edit.add(copy);
-        edit.add(paste);
-        edit.add(delete);
-        edit.add(selectAll);
-        menuBar.add(edit);
+        newObjectMenu.add(newGraphicItem);
+        newObjectMenu.add(newTerrainItem);
+        newObjectMenu.add(newPhysicsObjectItem);
+        newObjectMenu.add(newFighterItem);
+        fileMenu.add(newObjectMenu);
+        fileMenu.add(generateItem);
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(saveAsItem);
+        fileMenu.add(new JSeparator());
+        fileMenu.add(importAnimationItem);
+        fileMenu.add(new JSeparator());
+        fileMenu.add(setExeLocationItem);
+        menuBar.add(fileMenu);
         
-        object = new JMenu("Object");
-        objectAttributes = new JMenuItem("Attributes");
-        eventHolds = new JMenuItem("Event Holds");
-        objectAttributes.addActionListener(this);
-        eventHolds.addActionListener(this);
-        objectAttributes.setActionCommand("objectAttributes");
-        eventHolds.setActionCommand("eventHolds");
-        object.add(objectAttributes);
-        object.add(eventHolds);
-        menuBar.add(object);
+        editMenu = new JMenu("Edit");
+        //
+        undoItem = new JMenuItem("Undo");
+        undoItem.setActionCommand("undo");
+        undoItem.addActionListener(this);
+        undoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_DOWN_MASK));
+        //
+        redoItem = new JMenuItem("Redo");
+        redoItem.setActionCommand("redo");
+        redoItem.addActionListener(this);
+        //
+        cutItem = new JMenuItem("Cut");
+        cutItem.setActionCommand("cut");
+        cutItem.addActionListener(this);
+        cutItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK));
+        //
+        copyItem = new JMenuItem("Copy");
+        copyItem.setActionCommand("copy");
+        copyItem.addActionListener(this);
+        copyItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+        //
+        pasteItem = new JMenuItem("Paste");
+        pasteItem.setActionCommand("paste");
+        pasteItem.addActionListener(this);
+        pasteItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK));
+        //
+        deleteItem = new JMenuItem("Delete");
+        deleteItem.setActionCommand("delete");
+        deleteItem.addActionListener(this);
+        deleteItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
+        //
+        selectAllItem = new JMenuItem("Select All");
+        selectAllItem.setActionCommand("selectAll");
+        selectAllItem.addActionListener(this);
+        selectAllItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.ALT_DOWN_MASK));
+        //
+        
+        editMenu.add(undoItem);
+        editMenu.add(redoItem);
+        editMenu.add(new JSeparator());
+        editMenu.add(cutItem);
+        editMenu.add(copyItem);
+        editMenu.add(pasteItem);
+        editMenu.add(deleteItem);
+        editMenu.add(selectAllItem);
+        menuBar.add(editMenu);
+        
+        objectMenu = new JMenu("Object");
+        //
+        objectAttributesItem = new JMenuItem("Attributes");
+        objectAttributesItem.setActionCommand("objectAttributes");
+        objectAttributesItem.addActionListener(this);
+        //
+        eventHoldsItem = new JMenuItem("Event Holds");
+        eventHoldsItem.setActionCommand("eventHolds");
+        eventHoldsItem.addActionListener(this);
+        //
+        
+        objectMenu.add(objectAttributesItem);
+        objectMenu.add(eventHoldsItem);
+        menuBar.add(objectMenu);
         
         palettesMenu = new JMenu("Palettes");
         menuBar.add(palettesMenu);
         updatePalettesMenu();        
         
-        help = new JMenu("Help");
-        helpContent = new JMenuItem("Help Content");
-        about = new JMenuItem("About");
-        help.add(helpContent);
-        help.add(about);
-        menuBar.add(help);
+        helpMenu = new JMenu("Help");
+        //
+        helpContentItem = new JMenuItem("Help Content");
+        helpContentItem.setActionCommand("helpContent");
+        helpContentItem.addActionListener(this);
+        //
+        aboutItem = new JMenuItem("About");
+        aboutItem.setActionCommand("about");
+        aboutItem.addActionListener(this);
+        //
+        
+        helpMenu.add(helpContentItem);
+        helpMenu.add(aboutItem);
+        menuBar.add(helpMenu);
         
         setJMenuBar(menuBar);
     }
@@ -331,7 +330,7 @@ public class HoldListWindow extends JFrame implements ActionListener {
 	        Node root = doc.getDocumentElement();
 	        if(root.getNodeName().compareTo("Settings") != 0)
 	        {
-	        	JOptionPane.showMessageDialog(this, "Settings file has invalid root", "Error loading settings", JOptionPane.ERROR_MESSAGE); 
+	        	JOptionPane.showMessageDialog(this, "Settings file has invalid root, expected <Settings>", "Error loading settings", JOptionPane.ERROR_MESSAGE); 
 	        	return; 
 	        }
 	        
@@ -424,7 +423,7 @@ public class HoldListWindow extends JFrame implements ActionListener {
         }
         catch(Exception e)
         {
-        	
+        	JOptionPane.showMessageDialog(this, e.getMessage(), "Exception", JOptionPane.ERROR_MESSAGE); 
         }
     }
     
@@ -811,12 +810,12 @@ public class HoldListWindow extends JFrame implements ActionListener {
         return null;
     }
     
-    private String createAbsolutePath(String relPath)
+    public String createAbsolutePath(String relPath)
     {
     	return createAbsolutePathFrom(relPath, workingDirectory);
     }
     
-    private String createAbsolutePathFrom(String relPath, String fromPath)
+    public String createAbsolutePathFrom(String relPath, String fromPath)
     {
     	relPath = relPath.replace('\\', File.separatorChar);
     	relPath = relPath.replace('/', File.separatorChar);
@@ -832,13 +831,13 @@ public class HoldListWindow extends JFrame implements ActionListener {
 	    return absolute;
     }
     
-    private String createRelativePath(String absPath)
+    public String createRelativePath(String absPath)
     {
     	return createPathRelativeTo(absPath, workingDirectory);
     }
 
     
-    private String createPathRelativeTo(String absPath, String relativeTo)
+    public String createPathRelativeTo(String absPath, String relativeTo)
     {
     	String[] relativeToPieces = relativeTo.split(File.separator.compareTo("\\") == 0 ? "\\\\" : "/");
         String[] absPathPieces = absPath.split(File.separator.compareTo("\\") == 0 ? "\\\\" : "/");
@@ -2158,6 +2157,14 @@ public class HoldListWindow extends JFrame implements ActionListener {
         textureHitboxPane.textureHitboxPane.selectAll();
     }
     
+    private void helpContent() {
+    	
+    }
+    
+    private void about() {
+    	JOptionPane.showMessageDialog(this, "This is the object editor created for the Homestrife Fighting Game!\nOriginally created by Darlos9D and maintained by WhiteVoidia.", "About", JOptionPane.INFORMATION_MESSAGE);
+    }
+    
     private void createObjectAttributesWindow()
     {
     	if(currentlyLoadedObject == null) {
@@ -2255,6 +2262,8 @@ public class HoldListWindow extends JFrame implements ActionListener {
             case "objectAttributes": createObjectAttributesWindow(); break;
             case "eventHolds": createEventHoldsWindow(); break;
             case "palettes": createPalettesWindow(); break;
+            case "helpContent": helpContent(); break;
+            case "about": about();
         }
     }
     
